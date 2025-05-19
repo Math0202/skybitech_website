57c8bc3ff182eb93eefea4e4c65eb299128749e7
Tjitouua <veriimapoha@gmail.com>
2025-05-19 20:36:49 +0200

    Initial commit from Tjitouua

---
+++ index.js
@@ -0,0 +1,36 @@
+
+function IT_Clean() {
+    window.location.href = 'it_cleaning.html';
+}
+
+function computers() {
+    window.location.href = 'equipment.html';
+}
+
+function software() {
+    window.location.href = 'software.html';
+}
+
+function network() {
+    window.location.href = 'network.html';
+}
+
+function hosting() {
+    window.location.href = 'hosting.html';
+}
+
+function domain() {
+    window.location.href = 'domain.html';
+}
+
+function it_consulting() {
+    window.location.href = 'it_consultancy.html';
+}
+
+function repair() {
+    window.location.href = 'repair.html';
+}
+
+function printing() {
+    window.location.href = 'printing.html';
+}
